fd83c55f63d2405b15155c5d8a87542b7c0d0585
gjennison <georjennison@gmail.com>
2016-03-21 18:02:34 +1300

    Added files via upload
    I was unable to send a pull request because it said it was all up to date. Here is my source if you want to take a look

---
+++ HelloWorldApplication.java
@@ -0,0 +1,46 @@
+package com.driesdejager.tutorial.dropwizard;
+
+import com.driesdejager.tutorial.dropwizard.health.TemplateHealthCheck;
+import com.driesdejager.tutorial.dropwizard.resources.HelloWorldResource;
+import com.driesdejager.tutorial.dropwizard.resources.RegisterMinionResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+/**
+ * Created by driesd on 10/03/16.
+ */
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName(){
+        return "hello-world";
+    }
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap){
+
+    }
+
+    @Override
+    public void run (HelloWorldConfiguration configuration, Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        final RegisterMinionResource registerMinionResource = new RegisterMinionResource();
+        environment.jersey().register(registerMinionResource);
+        final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+
+
+    }
+
+
+
+
+}
+
